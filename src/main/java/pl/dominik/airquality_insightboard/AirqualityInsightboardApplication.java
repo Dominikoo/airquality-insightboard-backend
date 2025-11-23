cd268368490224c3c797578252f7317848a8c395
Dominikoo <pajor.dominik00@gmail.com>
2025-11-23 12:23:05 +0100

    Initial commit

---
+++ src/main/java/pl/dominik/airquality_insightboard/AirqualityInsightboardApplication.java
@@ -0,0 +1,13 @@
+package pl.dominik.airquality_insightboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirqualityInsightboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirqualityInsightboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dominik/airquality_insightboard/AirqualityInsightboardApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dominik.airquality_insightboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirqualityInsightboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
